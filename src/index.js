3f81a3128741acb04bf8983b8f8ce0bebe880f6e
ryantohhr <ryantohhongrui@hotmail.com>
2024-11-25 21:04:04 +0800

    Complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import "./style.css";
 import rain from "./assets/rain.svg";
 import partlyCloudyDay from "./assets/partly-cloudy-day.svg";
+import partlyCloudyNight from "./assets/partly-cloudy-night.svg";
 import cloudy from "./assets/cloudy.svg";
 import clearDay from "./assets/clear-day.svg";
 import clearNight from "./assets/clear-night.svg";
+import precipprobImgLink from "./assets/precipprobicon.svg";
+import sunsetImg from "./assets/sunset.svg";
+import sunriseImg from "./assets/sunrise.svg";
 
 
 async function getWeatherData(location) {
@@ -13,25 +17,45 @@ async function getWeatherData(location) {
     );
     if (response.ok) {
         const weatherData = await response.json();
-        console.log(weatherData);
         const appData = getAppData(weatherData);
-        console.log(appData);
         generateDOM(appData);
     }
 }
 
-getWeatherData("Singapore");
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        const input = document.querySelector("#search");
+        getWeatherData(input.value);
+    }
+})
 
 function getAppData(weatherData) {
     const temp = toCelsius(weatherData.currentConditions.temp).toFixed(1);
     const conditions = weatherData.currentConditions.conditions;
     const icon = weatherData.currentConditions.icon;
     const precipprob = weatherData.currentConditions.precipprob;
+    const sunset = weatherData.currentConditions.sunset.slice(0, 5);
+    const sunrise = weatherData.currentConditions.sunrise.slice(0, 5);
+    let sun;
+    const now = new Date();
+    const hoursNow = now.getHours().toString().padStart(2);
+    const minsNow = now.getMinutes().toString().padStart(2);
+    const timeNow = `${hoursNow}:${minsNow}`;
+    if (timeNow < sunrise) {
+        sun = "sunrise";
+    } else if (timeNow > sunset) {
+        sun = "sunrise";
+    } else {
+        sun = "sunset";
+    }
     return {
         temp, 
         conditions,
         icon,
-        precipprob
+        precipprob,
+        sunset,
+        sunrise,
+        sun,
     }
 }
 
@@ -40,7 +64,13 @@ function toCelsius(tempInFahrenheit) {
 }
 
 function generateDOM(appData) {
-    const dataDiv = document.querySelector("div.data");
+    const weatherDiv = document.querySelector(".weather");
+    while (weatherDiv.firstChild) {
+        weatherDiv.removeChild(weatherDiv.firstChild);
+    }
+    
+    const dataDiv = document.createElement("div");
+    dataDiv.classList.add("data");
 
     while (dataDiv.firstChild) {
         dataDiv.removeChild(dataDiv.firstChild);
@@ -54,6 +84,9 @@ function generateDOM(appData) {
             break;
         case "partly-cloudy-day":
             conditionsImg.src = partlyCloudyDay;
+            break;
+        case "partly-cloudy-night":
+            conditionsImg.src = partlyCloudyNight;
             break;
         case "cloudy":
             conditionsImg.src = cloudy;
@@ -76,4 +109,40 @@ function generateDOM(appData) {
     conditions.classList.add("conditions");
     conditions.textContent = `${appData.conditions}`;
     dataDiv.appendChild(conditions);
+
+    const otherInfo = document.createElement("div");
+    otherInfo.classList.add("otherinfo");
+
+    const precipprob = document.createElement("div");
+    precipprob.classList.add("precipprob");
+    const precipprobImg = document.createElement("img");
+    const precipprobText = document.createElement("div");
+    precipprobImg.src = precipprobImgLink;
+    precipprobText.textContent = `${appData.precipprob}%`;
+    precipprob.appendChild(precipprobImg);
+    precipprob.appendChild(precipprobText);
+
+    const sun = document.createElement("div");
+    sun.classList.add("sun");
+    const sunImg = document.createElement("img");
+    const sunText = document.createElement("div");
+    let suntext;
+    if (appData.sun === "sunset") {
+        sunImg.src = sunsetImg;
+        suntext = appData.sunset;
+    } else {
+        sunImg.src = sunriseImg;
+        suntext = appData.sunrise;
+    }
+    sunText.textContent = `${suntext}`
+    sun.appendChild(sunImg);
+    sun.appendChild(sunText);
+
+    otherInfo.appendChild(precipprob);
+    otherInfo.appendChild(sun);
+
+    dataDiv.appendChild(otherInfo);
+
+    weatherDiv.appendChild(dataDiv);
+
 }
